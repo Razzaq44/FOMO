3fd456de3255e4c9cf105b9861d07d104ca09f19
razzaq <razzaq.bowo@gmail.com>
2026-06-02 18:59:37 +0700

    fix: syntax error at product models and factory

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Database\Factories\ProductFactory;
 
 class Product extends Model
 {
+    /** @use HasFactory<ProductFactory> */
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
