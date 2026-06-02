ccb27b3dc4c38ef58df57ec58b4498cb3f32db1b
razzaq <razzaq.bowo@gmail.com>
2026-06-02 19:37:18 +0700

    feat: add to API routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CartController;
+use App\Http\Controllers\API\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/cart', [CartController::class, 'cartList']);
+    Route::post('/cart', [CartController::class, 'addToCart']);
+    Route::delete('/cart/{productId}', [CartController::class, 'removeFromCart']);
+});
