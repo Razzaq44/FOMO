da5df360d3104e4cb8fd82d1c6537d9270d4deae
razzaq <razzaq.bowo@gmail.com>
2026-06-02 18:55:57 +0700

    feat: add models, migrations, factories, and seeders for online shop

---
+++ app/Models/FlashSales.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FlashSales extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'flash_sale_price',
+        'flash_sale_stock',
+        'start_time',
+        'end_time',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
